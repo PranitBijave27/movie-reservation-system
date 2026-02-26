f660ac30fde3b7c249e38b63acb8d2c47e9db290
Pranit <pranitbijave27@gmail.com>
2026-02-26 14:31:46 +0530

    seats availabilty functionality added for booking

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
-
 const bookingController = require("../controllers/bookingController");
 
 router.post("/", bookingController.createBooking);
 router.get("/show/:showId/seats", bookingController.getBookedSeats);
+router.get("/show/:showId/availability",bookingController.getSeatAvailabilty);
 
 module.exports = router;
