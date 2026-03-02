82f0a5159732ba66fdd23a16359fe5aa3f329785
Pranit <pranitbijave27@gmail.com>
2026-03-02 23:25:35 +0530

    Added user booking history API

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -8,4 +8,12 @@ router.get("/show/:showId/availability",bookingController.getSeatAvailabilty);
 router.patch("/:bookingId/confirm",bookingController.confirmBooking);
 router.patch("/:bookingId/cancel",bookingController.cancelBooking);
 
+router.get("/me",bookingController.getMyBookings
+);
+
+// temporary for testing
+router.get(
+  "/user/:userId",
+  bookingController.getMyBookings
+);
 module.exports = router;
