4371273087d441fcca03175b1d098a36d96d62ec
Pranit <bijwepranit@gmail.com>
2026-03-03 16:04:54 +0530

    fix: prevented race condition while creating booking

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,68 +1,81 @@
 const Seat=require("../models/Seat");
 const Booking = require("../models/Booking");
 const Show = require("../models/Show");
+const mongoose=require("mongoose");
 
 exports.createBooking = async ({ userId, showId, seatIds }) => {
 
-    if(seatIds.length>10){
-        throw new Error("More that 10 seats booking not allowed");
+    const session =await mongoose.startSession();
+    session.startTransaction();
+    try{
+
+        if(seatIds.length>10){
+            throw new Error("More than 10 seats booking are not allowed");
+        }
+        const show = await Show.findById(showId); // show verification
+
+        if(!show){
+            throw new Error("Show not found");
+        }
+
+        if(show.status !== "scheduled")
+            throw new Error("Show not available");
+
+        if(show.startTime <=new Date()) {
+            throw new Error("sorry either show already started or completed");
+        }
+        
+        const seats=await Seat.find({
+            _id:{$in:seatIds},
+            screenId:show.screenId
+        });
+
+        if (seats.length !== seatIds.length)
+            throw new Error("Invalid seats selected");
+
+        const existing = await Booking.findOne({
+            showId,
+            seats: { $in: seatIds },
+            status: { $in: ["pending","confirmed"] } //Pending These are seats currently in someone's "cart"
+        });
+
+        if (existing)
+            throw new Error("Some seats already booked");
+
+        
+        let totalAmount = seats.reduce((sum, seat) => {
+            let multiplier = 1;
+            if(seat.type === "premium") multiplier =1.45;
+            if(seat.type === "vip") multiplier = 1.75;
+
+            return sum + show.basePrice * multiplier;
+        },0);
+
+        totalAmount = Math.round(totalAmount);
+        totalAmount = Math.ceil(totalAmount);
+        
+        if (totalAmount % 10 === 0) {
+            totalAmount = totalAmount - 1;
+        }
+        //5 minutes lock for seat
+        const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
+
+        const booking=await Booking.create({
+            userId,
+            showId,
+            seats:seatIds,
+            totalAmount,
+            expiresAt
+        });
+        await session.commitTransaction();
+        session.endSession();
+
+        return booking[0];
+    }catch(err){
+        await session.abortTransaction();
+        session.endSession();
+        throw err;
     }
-    const show = await Show.findById(showId); // show verification
-
-    if(!show){
-        throw new Error("Show not found");
-    }
-
-    if(show.status !== "scheduled")
-        throw new Error("Show not available");
-
-    if(show.startTime <new Date()) {
-        throw new Error("sorry either show already started or completed");
-    }
-    
-    const seats=await Seat.find({
-        _id:{$in:seatIds},
-        screenId:show.screenId
-    });
-
-    if (seats.length !== seatIds.length)
-        throw new Error("Invalid seats selected");
-
-    const existing = await Booking.findOne({
-        showId,
-        seats: { $in: seatIds },
-        status: { $in: ["pending","confirmed"] } //Pending These are seats currently in someone's "cart"
-    });
-
-    if (existing)
-        throw new Error("Some seats already booked");
-
-    
-    let totalAmount = seats.reduce((sum, seat) => {
-        let multiplier = 1;
-        if(seat.type === "premium") multiplier =1.45;
-        if(seat.type === "vip") multiplier = 1.75;
-
-        return sum + show.basePrice * multiplier;
-    },0);
-
-    totalAmount = Math.round(totalAmount);
-    totalAmount = Math.ceil(totalAmount);
-    
-    if (totalAmount % 10 === 0) {
-        totalAmount = totalAmount - 1;
-    }
-    //5 minutes lock for seat
-    const expiresAt = new Date(Date.now() + 5 * 60 * 1000);
-
-    const booking=await Booking.create({
-        userId,
-        showId,
-        seats:seatIds,
-        totalAmount,
-        expiresAt
-    });
-    return booking;
 }
 
 
