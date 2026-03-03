d263c7fc3af22adb812419ea842104468491f0f3
Pranit <bijwepranit@gmail.com>
2026-03-03 16:43:39 +0530

    cleanup: remove old root src after backend restructuring

--- src/controllers/screenController.js
+++
@@ -1,8 +0,0 @@
-const screenService = require("../services/screenService");
-const wrapAsync = require("../utils/wrapAsync");
-
-
-exports.createScreen =wrapAsync( async (req,res,next)=>{
-    const screen = await screenService.createScreen(req.body);
-    res.status(201).json(screen);
-});
