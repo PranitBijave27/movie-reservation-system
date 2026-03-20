3804f1838d9138045bd4cac4559720d3fe30561d
Pranit Bijave <bijwepranit@gmail.com>
2026-03-20 23:34:52 +0530

    model changed

--- backend/src/utils/gemini.js
+++ backend/src/utils/gemini.js
@@ -5,7 +5,7 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 const getGeminiRecommendation = async (prompt) => {
   try {
     const result = await ai.models.generateContent({
-      model: "gemini-1.5-flash", 
+      model: "gemini-3-flash-preview", 
       contents: [{ role: "user", parts: [{ text: prompt }] }],
     });
     return result.text;
